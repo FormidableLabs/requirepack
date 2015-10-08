f679dd883fe2c877c27ddcb0cd8fa4bf852c8c2a
Ryan Roemer <ryan@loose-bits.com>
2015-10-08 16:32:21 +0200

    Get everything actually mapped for manifest. Still have multiple errors (hbs version, 'normalize' plugin RJS error).

--- test/func/complex-amd/build.js
+++ test/func/complex-amd/build.js
@@ -3,16 +3,13 @@
 /**
  * Build test scenario (HTML, JS).
  */
+var _ = require("lodash");
+var webpackConfig = require("./webpack.config.lib");
 var Build = require("../../util/build");
+
 var build = module.exports = new Build({
   rootDir: __dirname,
   requirepack: {
-    empty: [
-      "hbs"
-    ],
-    alias: {
-      "hbs/handlebars": "handlebars/runtime"
-    },
     // Transforms occur in order and do not stop after first match.
     transforms: [
       // Match the NormalModuleReplacementPlugin path switch we do in Webpack
@@ -23,11 +20,33 @@ var build = module.exports = new Build({
           return name.slice(4) + ".hbs";
         }
       }
+    ],
+
+    // Aliases match whole tokens or partial path prefixes at file-separator
+    // barriers.
+    alias: _.extend({
+      // RequireJS -> Webpack aliases.
+      "hbs/handlebars": "handlebars/runtime"
+
+      // Direct import the webpack aliases. Currently, this gives us:
+      // - `custom-alias`
+      //
+      // **Note**: This only works for easy, straight aliases, not things like
+      // `$` suffixed aliases in
+      // https://webpack.github.io/docs/configuration.html#resolve-alias
+      //
+      // See: https://github.com/FormidableLabs/requirepack/issues/11
+      //
+      // Also note that we could _instead_ do via manual `transforms`.
+    }, webpackConfig.resolve.alias),
+
+    // Empties are whole token matches only.
+    empty: [
+      "hbs"
     ]
   }
 });
 
 if (require.main === module) {
-  //TODObuild.run();
-  build.buildRequirePack(); // TODO REMOVE & PLACE IN test `before`
+  build.run();
 }
